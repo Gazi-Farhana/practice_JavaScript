3ab1d819eef441295a8695f4fc87d4c48e2e14c2
Gazi-Farhana <gazifarhana95@gmail.com>
2023-06-21 14:31:18 +0600

    multistage conditions

---
+++ multiStageCondition.js
@@ -0,0 +1,20 @@
+var book1 = 100;
+var book2 = 200;
+var book3 = 300;
+var book4 = 400;
+var book5 = 500;
+ if (book1 > book2){
+    console.log('I will buy book2');
+ }
+ else if(book2 > book3){
+    console.log('I will buy book3');
+ }
+ else if(book3 > book4){
+    console.log('I will buy book3');
+ }
+ else if(book4 < book5){
+    console.log('I will buy book4');
+ }
+ else{
+    console.log('I will buy book5');
+ }
