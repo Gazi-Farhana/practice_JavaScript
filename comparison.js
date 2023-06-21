8f5162d09a77c692d4a6bb2e2c469cdd0cf836e1
Gazi-Farhana <gazifarhana95@gmail.com>
2023-06-21 09:02:44 +0600

    17-5 done

---
+++ comparison.js
@@ -0,0 +1,17 @@
+//  less than <
+console.log(5 < 6);
+// greater than >
+console.log(5 > 8);
+// less than equal:
+console.log( 1 <= 2);
+// greater than equal >=
+console.log(4 >= 4);
+// equal ==
+console.log(7==7);
+console.log(7==7.5);
+// not equal !=
+console.log(7!=9);
+
+// Multiple conditions:
+// money1 > money2 && result1 > result2 && height1 > height2
+// money1 > money2 || result1 > result2 || height1 > height2
