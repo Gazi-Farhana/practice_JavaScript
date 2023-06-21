a01020fa66b207f53e9621d2eaa29f28f5868955
Gazi-Farhana <gazifarhana95@gmail.com>
2023-06-21 09:26:54 +0600

    18-6 done

---
+++ conditions.js
@@ -0,0 +1,22 @@
+var phone = 75000;
+var budget = 50000;
+if(phone < budget){
+    // console.log('I will purchase the phone');
+}
+
+var laptop = 60000;
+var savings = 70000;
+if(laptop < savings){
+    // console.log('I will buy the laptop');
+}
+
+
+//  if-else conditions:
+var lunch = 5000;
+var myMoney = 350;
+if(lunch < myMoney){
+    console.log('i will buy this lunch');
+}
+else{
+    console.log('i will cook my food at home');
+}
