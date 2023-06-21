7e5e3306ca67a598e4a778c157e52e1d3ad4612a
Gazi-Farhana <gazifarhana95@gmail.com>
2023-06-21 12:40:00 +0600

    18-7 Done

---
+++ multipleConditions.js
@@ -0,0 +1,18 @@
+// var isGraduateed = true;
+// var salary = 25000;
+// if(isGraduateed == true && salary > 50000){
+//     console.log('I will start Masters');
+// }
+// else{
+//     console.log('i will postpond my Masters degree');
+// }
+
+var isGraduateed = true;
+var job = 1;
+var salary = 25000;
+if(isGraduateed === true || salary > 50000 && job == 1){
+    console.log('I will start Masters');
+}
+else{
+    console.log('i will postpond my Masters degree');
+}
